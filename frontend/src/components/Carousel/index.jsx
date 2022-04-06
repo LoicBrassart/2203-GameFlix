4d8bd0f81c735f2e2eaf42befac2e2afb153aae7
LoicBrassart <loic.brassart@gmail.com>
2022-04-06 02:39:19 +0200

    Add basic Carousel [broken]

--- frontend/src/components/Carousel/index.jsx
+++ frontend/src/components/Carousel/index.jsx
@@ -1,5 +1,40 @@
+import { Pagination, EffectCoverflow } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-coverflow";
+
+import img1 from "@assets/carousel/01.jpg";
+import img2 from "@assets/carousel/02.jpg";
+import img3 from "@assets/carousel/03.jpg";
 import SCarousel from "./style";
 
 export default function Carousel() {
-  return <SCarousel>I iz a Carousel</SCarousel>;
+  return (
+    <SCarousel>
+      <Swiper
+        modules={[Pagination, EffectCoverflow]}
+        slidesPerView={2}
+        navigation
+        grabCursor="true"
+        centeredSlides="true"
+        loop="true"
+        pagination={{ clickable: true, dynamicBullets: true }}
+        coverflowEffect={{
+          rotate: 30,
+          stretch: 25,
+          depth: 250,
+        }}
+        effect="coverflow"
+      >
+        {[img1, img2, img3, img1, img2, img3, img1, img2, img3].map(
+          (pic, idx) => (
+            <SwiperSlide className="slide" key={idx}>
+              <img src={pic} alt="" />
+            </SwiperSlide>
+          )
+        )}
+      </Swiper>
+    </SCarousel>
+  );
 }
--- frontend/src/components/Carousel/style.jsx
+++ frontend/src/components/Carousel/style.jsx
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 
-export default styled.section``;
+export default styled.section`
+  .slide {
+    box-sizing: border-box;
+    border: 2px dotted yellowgreen;
+
+    & > * {
+      width: 100%;
+    }
+  }
+`;
