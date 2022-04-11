2c57e8510ed597b1cc198f2508deb7066d793b60
LoicBrassart <loic.brassart@gmail.com>
2022-04-11 21:08:27 +0200

    Ad a Toggler in the Header to switch between domains (rpg vs tabletop)

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,9 +1,11 @@
 import SNavBar from "./style";
+import Toggle from "@components/Toggle";
 
 export default function NavBar() {
   return (
     <SNavBar>
       <img src="" alt="" />
+      <Toggle />
       <nav>
         <ul>
           <li>Jeux</li>
