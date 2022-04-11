4332ac2e22fa92352363595e3af210e5f1ef6e17
LoicBrassart <loic.brassart@gmail.com>
2022-04-11 21:11:26 +0200

    Add support for Games table

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { ItemController } = require("./controllers");
+const { ItemController, GameController } = require("./controllers");
 
 const router = express.Router();
 
@@ -9,5 +9,11 @@ router.get("/items/:id", ItemController.read);
 router.put("/items/:id", ItemController.edit);
 router.post("/items", ItemController.add);
 router.delete("/items/:id", ItemController.delete);
+
+router.get("/games", GameController.browse);
+router.get("/games/:id", GameController.read);
+router.put("/games/:id", GameController.edit);
+router.post("/games", GameController.add);
+router.delete("/games/:id", GameController.delete);
 
 module.exports = router;
