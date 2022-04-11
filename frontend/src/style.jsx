4f840f3ff93c5508d5a94a27b42364f468559b3b
LoicBrassart <loic.brassart@gmail.com>
2022-04-11 21:07:20 +0200

    Add whitespace

--- frontend/src/style.jsx
+++ frontend/src/style.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export default styled.div`
   box-sizing: border-box;
   min-height: 100vh;
+
   main {
     min-height: 90vh;
     background: url("https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.CDr_SlBrNymTKfbleckFYwHaE7%26pid%3DApi&f=1"),
